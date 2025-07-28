651edef7ddc144fe299eaf5ca3f0cacbfd4b009a
KandulaVamshidharRed <Vamsidhar.Kanudula@ibm.com>
2025-07-28 21:05:06 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,16 +19,7 @@ import { SidebarUserButton } from "@/features/users/components/SidebarUserButton
 import { insertUser } from "@/features/db/users";
 
 export default async function HomePage() {
-      // const user =  await insertUser({
-      //             id: "1",
-      //             name: `vamsidhar kandula`,
-      //             imageUrl: "https://img.clerk.com/eyJ0eXBlIjoiZGVmYXVsdCIsImlpZCI6Imluc18zMEJhUzJIRzh1UU9RWXQ3ZzJjSHBLN2t6TzkiLCJyaWQiOiJ1c2VyXzMwVk93c3NCdXM0d0cybHNlMUl4akc3MWZwMCIsImluaXRpYWxzIjoiVksifQ",
-      //             email: "vamsimay1234@gmail.com",
-      //             createdAt: new Date(1753709514528),
-      //             updatedAt: new Date(1753709514551)
-      //         })
-      // console.log("inserted user", user);         
-      console.log("created_at", typeof(new Date(1753709514528))) 
+
   return (
     <SidebarProvider className="overflow-y-hidden">
       <AppSidebarClient>
@@ -66,12 +57,7 @@ export default async function HomePage() {
          </SignedIn>
         </Sidebar>
       </AppSidebarClient>
-      <main className="flex-1">main page content
-
-        <div className="pt-3">
-          <button>click me</button>
-        </div>
-      </main>
+      <main className="flex-1">main page content</main>
     </SidebarProvider>
   );
 }
