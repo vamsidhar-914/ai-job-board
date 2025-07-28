2012e8346e0266656b68b11dcbc9407d649d0d78
KandulaVamshidharRed <Vamsidhar.Kanudula@ibm.com>
2025-07-28 21:13:31 +0530

    env variables fix

--- src/data/env/server.ts
+++ src/data/env/server.ts
@@ -4,16 +4,16 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     CLERK_SECRET_KEY: z.string().min(1),
-    CLERK_WEBHOOK_SECRET: z.string().min(1)
+    CLERK_WEBHOOK_SECRET: z.string().min(1),
+    DATABASE_URL: z.string().min(1)
   },
   createFinalSchema: (env) => {
     return z.object(env).transform((val) => {
-      const { ...rest } = val;
+      const { DATABASE_URL,...rest } = val;
 
       return {
         ...rest,
-        DATABASE_URL: "postgresql://postgres.jkeuvylzejqtiarwqtcc:vamsidhar@123@aws-0-us-east-1.pooler.supabase.com:6543/postgres"
-,
+        DATABASE_URL: DATABASE_URL
       };
     });
   },
